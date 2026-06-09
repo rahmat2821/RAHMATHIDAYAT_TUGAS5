fc2d86cb9cfe798072dc48972781db7e8e15efe0
rahmat2821 <rahmatthidayattt2006@gmail.com>
2026-06-10 00:29:17 +0700

    menambahkan index

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Belajar PHP</title>
+</head>
+<body>
+
+    <h1>Praktikum PHP</h1>
+
+    <?php
+    echo "<p>Selamat belajar PHP</p>";
+    ?>
+
+</body>
+</html>
+
+
